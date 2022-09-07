7ccfa061d4e31135cf6b81065a637a6b6bad295d
akmalrafidiara <akmalrafidiara@gmail.com>
2022-09-07 15:16:34 +0700

    add remove active

--- script.js
+++ script.js
@@ -1,6 +1,13 @@
+AOS.init();
+
 const navBtn = document.querySelector('.icon');
 const nav = document.querySelector('.navbar');
+const navItem = document.querySelector('.navbar')
 
 navBtn.addEventListener('click', function() {
   nav.classList.toggle("active");
+})
+
+navItem.addEventListener('click', function() {
+  nav.classList.remove("active");
 })
