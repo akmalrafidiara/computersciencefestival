6a6a306a8ebdb7814d84c755c3f9450c904dce89
akmalrafidiara <akmalrafidiara@gmail.com>
2022-09-07 13:47:48 +0700

    update small

--- script.js
+++ script.js
@@ -4,11 +4,3 @@ const nav = document.querySelector('.navbar');
 navBtn.addEventListener('click', function() {
   nav.classList.toggle("active");
 })
-
-// const header = document.querySelector("header");
-//     const banner = document.querySelector(".banner");
-//     window.addEventListener("scroll", function () {
-//       header.classList.toggle("sticky", window.scrollY > 0);
-//       var value = window.scrollY;
-//       banner.style.top = value * 0.3 + "px";
-//     });
